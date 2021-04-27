ff126864f1c4ec2d46bb6d20cba00ffdde276457
BShaw274 <bts9756@gmail.com>
2021-04-27 17:42:56 -0400

    Fixed the study materials to work

--- PHPandSQL/getStudyMaterials.php
+++ PHPandSQL/getStudyMaterials.php
@@ -1,7 +1,8 @@
 <?php
-$Email = $_POST['email'];
-//$Email = "parent1@gmail.com";
+//$Email = $_POST['email'];
+$Email = "student7@gmail.com";
 
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $dbConnection = new mysqli('localhost', 'root', '', 'db2');
 if ($dbConnection->connect_error) {
   die("Connection failed: " . $dbConnection->connect_error);
@@ -49,60 +50,45 @@ $query  = "SELECT material_id FROM assign
 INNER JOIN meetings ON assign.meet_id = meetings.meet_id
 WHERE meetings.date >= '{$previousWeek->format('Y-m-d')}' AND meetings.date <= '{$currentWeek->format('Y-m-d')}'";
 $results = mysqli_query($dbConnection, $query);
+//var_dump($results);
 
 $materialIds = [];
 while ($row = mysqli_fetch_array ($results, MYSQLI_ASSOC)) {
   //echo $meetingGrade;
   array_push($materialIds, $row['material_id']);
 }
-
+//var_dump($materialIds);
 $stmt->close();
 //echo "|||||||||||Var Dump Start";
 
 //echo "Var Dump over|||||||||||";
 
 $response = array();
-  //get all info of users with id with for loop
-  for ($i=0; $i<count($materialIds);$i++){
+  //gets all the study material information
   
+    for ($i = 0; $i < mysqli_num_rows($results); $i++){
+      $query = "SELECT * FROM material WHERE material_id = {$materialIds[$i]}";
+      $result = mysqli_query($dbConnection, $query);
+      while($row = mysqli_fetch_assoc($result)){
+        $response[strval($i) . "cid"] = $row['material_id'];
+        $response[strval($i) . "cTitle"] = $row['title'];
+        $response[strval($i) . "cAuthor"] = $row['author'];
+        $response[strval($i) . "cType"] = $row['type'];
+        $response[strval($i) . "cUrl"] = $row['url'];
+        $response[strval($i) . "cAssigned_date"] = $row['assigned_date'];
+        $response[strval($i) . "cNotes"] = $row['notes'];
+      }
+    }
+        
+    
 
-$stmt = $dbConnection->prepare("SELECT * FROM material Where id = ?");
-if(false ===$stmt){
-  //die('prepare() failed: ' . htmlspecialchars($mysqli->error));
-  $response["success"] = "false";
-    echo json_encode($response);
-//echo " Echoing Email update: ".$response["success"];
-}
-$check = $stmt->bind_param("s", $materialIds[$i]['material_id']);
-if(false ===$check){
-  //die('bind_param() failed: ' . htmlspecialchars($stmt->error));
-  $response["success"] = "false";
-    echo json_encode($response);
-//echo " Echoing Email update: ".$response["success"];
-}
-$check = $stmt->execute();
-if(false ===$check){
-  //die('execute() failed: ' . htmlspecialchars($stmt->error));
-    $response["success"] = "false";
-      echo json_encode($response);
-}
-$MaterialsInfo = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-$stmt->close();
-
-//var_dump($StudentsInfo);
-// get the child info that correspond to the student id's
-//$getUserInfo = "SELECT id, name, email, phone FROM users";
-//$infoRes = $mysqli->query($getUserInfo);
-//echo $i."|||";
-
-$response[strval($i) . "cid"] = $MaterialsInfo[0]['material_id'];
-$response[strval($i) . "cTitle"] = $MaterialsInfo[0]['title'];
-$response[strval($i) . "cAuthor"] = $MaterialsInfo[0]['author'];
-$response[strval($i) . "cType"] = $MaterialsInfo[0]['type'];
-$response[strval($i) . "cUrl"] = $MaterialsInfo[0]['url'];
-$response[strval($i) . "cAssigned_date"] = $MaterialsInfo[0]['assigned_date'];
-//var_dump($response);
-}// for close
+    //var_dump($StudentsInfo);
+    // get the child info that correspond to the student id's
+    //$getUserInfo = "SELECT id, name, email, phone FROM users";
+    //$infoRes = $mysqli->query($getUserInfo);
+    //echo $i."|||";
+  //var_dump($response);
+// for close
 //echo"||||||||||||End Result|||||||||";
 echo json_encode($response);
 ?>
